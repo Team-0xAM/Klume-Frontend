2082007692452c82ee99543b4c30947861ab589f
jiruen <kjr2699@naver.com>
2025-11-08 22:06:44 +0900

    chore: #1 - vue 초기 세팅

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterView } from 'vue-router';
+</script>
+
+<template>
+  <!-- <Header /> -->
+  <RouterView />
+  <!-- <Footer /> -->
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Klume Home</h1>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
